54d38667fef26f9701bd6ebebcf149d019231571
Jazzy <jasmina.gasper@gmail.com>
2016-04-12 15:55:02 +0100

    added reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews
+  end
 
 
 
---
+++ spec/models/restaurant_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe Restaurant, type: :model do
+  it { is_expected.to have_many :reviews }
+end
