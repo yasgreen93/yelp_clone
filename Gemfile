f55bb1c87f6ec273a2b00e91cd4a95c0acc25dfc
Jazzy <jasmina.gasper@gmail.com>
2016-04-13 15:17:28 +0100

    facebook auth

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'haml'
 gem 'shoulda'
+gem 'omniauth-facebook'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 # Use postgresql as the database for Active Record
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.secret_key = '0525a786e94b62080c3be8d5faf1841cccd6748720ebb41205bfaa993bc408cb7e5a7c6c891a629183a432453992c54d6f5e1d9d22d42d976ec3cfa117babc4a'
+  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"],
+                    callback_url: "http://localhost:3000"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
