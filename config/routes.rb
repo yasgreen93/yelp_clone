9fc5dfeddbec50dca45846e424714f74586b562f
Hanif Fakira <fakira.work@gmail.com>
2016-04-13 12:21:37 +0100

    settings for devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews
   end
-
+  root "restaurants#index"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
